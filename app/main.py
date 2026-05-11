16056bf359c68bb4713ce92c73858ce8c4f4e90c
Gopi Krishnan <gopigopikrishnand@gmail.com>
2026-05-12 00:18:25 +0530

    implemented security

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from app.database import engine, Base
 from app.models.student import Student
 from app.routes.student_routes import router as student_router
 
+from app.models.user import User
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
